44574af5aa8c720f88cba8e014657a0d22068234
曾梦想仗剑走天涯 <2878358795@qq.com>
2023-03-18 17:58:14 +0800

    第三次次提交

---
+++ xuecheng-plus-base/src/test/java/tang/xuechengplusbase/XuechengPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package tang.xuechengplusbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
